237306a0429bb8c702da174fed16ef0b160845eb
solid-matrix <antenna3mt@outlook.com>
2024-05-20 23:20:42 +0800

    test notify

---
+++ pages/lab/test-notify.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const notifyPermit = ref(false);
+
+onMounted(async () => {
+    const res = await Notification.requestPermission();
+    if (res) {
+        notifyPermit.value = true;
+    }else{
+        notifyPermit.value = false;
+    }
+});
+
+
+const notify = ()=>{
+    const notify = new Notification('Hello');
+}
+
+</script>
+
+<template>
+    <h1>Test Notification</h1>
+    <div>Permit: {{ notifyPermit? 'YES':'NO' }}</div>
+    <div v-if="notifyPermit">
+        <button @click="notify">Notify</button>
+    </div>
+</template>
