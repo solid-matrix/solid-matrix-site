a039374b6495a0c3d8a595b570e6cbb6bc82789f
solid-matrix <antenna3mt@outlook.com>
2024-05-20 23:35:50 +0800

    test service worker notify

--- pages/lab/test-notify.vue
+++ pages/lab/test-notify.vue
@@ -1,18 +1,28 @@
 <script setup lang="ts">
+
 const notifyPermit = ref(false);
 
+let registration:ServiceWorkerRegistration;
+
 onMounted(async () => {
+    navigator.serviceWorker.register('/sw.js');
+
     const res = await Notification.requestPermission();
-    if (res) {
+    if (res === 'granted') {
         notifyPermit.value = true;
+        registration = await navigator.serviceWorker.ready;
+
     }else{
         notifyPermit.value = false;
     }
+
 });
 
 
 const notify = ()=>{
-    const notify = new Notification('Hello');
+    //const notify = new Notification('Hello');
+
+    registration.showNotification('Hello');
 }
 
 </script>
